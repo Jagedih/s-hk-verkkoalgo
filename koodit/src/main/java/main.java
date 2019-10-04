7ff1dfb19f278d500108b773db54c2e80f7717f7
Jagedih <Jagedih@users.noreply.github.com>
2019-10-05 02:12:52 +0300

    Add files via upload

--- koodit/src/main/java/main.java
+++ koodit/src/main/java/main.java
@@ -1,23 +1,17 @@
 
-import Hakualgoritmit.Syvyyshaku;
+import Algoritmit.Syvyyshaku;
 import SyoteGeneraattori.SyoteGen;
 import Verkkokomponentit.Verkko;
 
 public class main{
     
     public static void main(String [ ] args){
-        
-        SyoteGen syotteet = new SyoteGen(300,500,0); //kaikilla kaarilla yhteys  verkkoon
+        //100 solmua, 300 kaarta, 70% todennäköisyys johdon sähköttömyydelle
+        SyoteGen syotteet = new SyoteGen(100,300,70); 
         Verkko verkko = new Verkko();
         verkko.lataaSyote(syotteet.kaariLista);
         verkko.rakennaVerkko();
         Syvyyshaku syvyysAlgo = new Syvyyshaku(verkko);
-        System.out.println("---------------------------------------------");
-        syotteet = new SyoteGen(300,500,30); //30% todennäköisyys että voimajohdolla ei ole sähköä
-        verkko = new Verkko();
-        verkko.lataaSyote(syotteet.kaariLista);
-        verkko.rakennaVerkko();
-        syvyysAlgo = new Syvyyshaku(verkko);
-        
+        syvyysAlgo.tulostaAliverkot();
     }
 }
